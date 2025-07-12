30fb423cfae74ccc14ab8a52478c1fc8cb764edf
kappader4 <kappader4@gmail.com>
2025-07-12 13:19:14 +0200

    Fixed Bounty glitch

--- src/main/java/net/kappasmp/kappaessentials/Kappaessentials.java
+++ src/main/java/net/kappasmp/kappaessentials/Kappaessentials.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.kappasmp.kappaessentials.bounty.BountyCommand;
 import net.kappasmp.kappaessentials.bounty.BountyManager;
+import net.kappasmp.kappaessentials.bounty.BountyRewardHandler;
 import net.kappasmp.kappaessentials.command.*;
 import net.kappasmp.kappaessentials.config.ConfigManager;
 import net.kappasmp.kappaessentials.economy.BalCommand;
@@ -57,6 +58,8 @@ public class Kappaessentials implements ModInitializer {
 		BountyManager.loadBounties();
 
 		HomeManager.loadHomes();
+
+		BountyRewardHandler.register();
 
 		ServerTickEvents.END_SERVER_TICK.register(HomeTeleportScheduler::tick);
 
