bcec9237333a92de2bf4e7bb7c9c2b3ac75647fa
kappader4 <kappader4@gmail.com>
2025-07-13 08:11:41 +0200

    Fixed Issues with Bounty System - 1.21

--- src/main/java/net/kappasmp/kappaessentials/Kappaessentials.java
+++ src/main/java/net/kappasmp/kappaessentials/Kappaessentials.java
@@ -8,6 +8,7 @@ import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.kappasmp.kappaessentials.bounty.BountyCommand;
 import net.kappasmp.kappaessentials.bounty.BountyManager;
+import net.kappasmp.kappaessentials.bounty.BountyRewardHandler;
 import net.kappasmp.kappaessentials.command.*;
 import net.kappasmp.kappaessentials.config.ConfigManager;
 import net.kappasmp.kappaessentials.economy.BalCommand;
@@ -59,7 +60,7 @@ public class Kappaessentials implements ModInitializer {
 		log("Mod initialized.");
 
 		BountyManager.loadBounties();
-
+		BountyRewardHandler.register();
 		HomeManager.loadHomes();
 
 		ServerTickEvents.END_SERVER_TICK.register(HomeTeleportScheduler::tick);
@@ -69,7 +70,7 @@ public class Kappaessentials implements ModInitializer {
 		System.out.println("[KappaEssentials] Initialized with config:");
 		System.out.println(ConfigManager.getHomeConfig()); // Debug print
 
-		ModUpdateChecker.checkForUpdates("1.0.0-1.21");
+		ModUpdateChecker.checkForUpdates("1.0.2-1.21");
 	}
 
 	private void registerCommands() {
@@ -100,7 +101,7 @@ public class Kappaessentials implements ModInitializer {
 			ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
 				ServerPlayerEntity player = handler.player;
 				if (player.hasPermissionLevel(2)) {
-					ModUpdateChecker.notifyIfOutdated(player, "1.1.5");
+					ModUpdateChecker.notifyIfOutdated(player, "1.0.1-1.21");
 				}
 			});
 		});
